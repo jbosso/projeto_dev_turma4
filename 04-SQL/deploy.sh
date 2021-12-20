2b20f26e4c018197b5c7e7fcc4dc3ee252d8a387
jbosso <j_bosso@outlook.com>
2021-12-20 14:10:06 -0300

    message

--- 04-SQL/deploy.sh
+++ 04-SQL/deploy.sh
@@ -3,7 +3,7 @@
 # -- Variaveis AWS
 uri='ssh -i /var/lib/jenkins/.ssh/id_rsa ubuntu@52.67.249.96'
 
-export TF_VAR_vpcId=$($uri 'aws --region sa-east-1 ec2 describe-vpcs --filters Name=tag:Name,Values=vpc-Team4-JB --query "Vpcs[*].VpcId" --output text')
+export TF_VAR_vpcId=$($uri 'aws --region sa-east-1 ec2 describe-vpcs --filters Name=tag:Name,Values=vpc-Team4 --query "Vpcs[*].VpcId" --output text')
 export TF_VAR_subnetPrivada=$($uri 'aws --region sa-east-1 ec2 describe-subnets --filters "Name=vpc-id,Values='$TF_VAR_vpcId'" "Name=tag:Name,Values=*Priv*" --query "Subnets[1].SubnetId" --output text')
 export TF_VAR_keyPrivate='jb-key'
 export TF_VAR_amiId='ami-0e66f5495b4efdd0f'
