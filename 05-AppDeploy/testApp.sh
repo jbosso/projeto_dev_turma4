014249c811659ca92ce06c5fc846e222e04d2e32
jbosso <j_bosso@outlook.com>
2021-12-20 09:47:42 -0300

    message

--- 05-AppDeploy/testApp.sh
+++ 05-AppDeploy/testApp.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# -- Variaveis AWS
+uri='ssh -i /var/lib/jenkins/.ssh/id_rsa ubuntu@52.67.249.96'
+
+export SQLdev=$($uri 'aws --region sa-east-1 ec2 describe-instances --filters Name=tag:Name,Values=mysql-dev --query "Reservations[*].Instances[*].[PrivateIpAddress]" --output text')
+# -- 
+
 export USER='root'
 export PASSWORD=\"$DB_PASSWORD\" 
-export DATABASE_URL='mysql://192.168.13.102:3306/SpringWebYoutubeTest?useTimezone=true&serverTimezone=UTC'
+export DATABASE_URL='mysql://$SQLdev:3306/SpringWebYoutubeTest?useTimezone=true&serverTimezone=UTC'
 
 echo $USER
 echo $PASSWORD
