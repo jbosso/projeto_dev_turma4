e94d0a96d7bc46dcfbca4a8f47d32cc093dc8d7f
jbosso <j_bosso@outlook.com>
2021-12-20 09:50:12 -0300

    message

--- 05-AppDeploy/testApp.sh
+++ 05-AppDeploy/testApp.sh
@@ -5,7 +5,6 @@ uri='ssh -i /var/lib/jenkins/.ssh/id_rsa ubuntu@52.67.249.96'
 
 export SQLdev=$($uri 'aws --region sa-east-1 ec2 describe-instances --filters Name=tag:Name,Values=mysql-dev --query "Reservations[*].Instances[*].[PrivateIpAddress]" --output text')
 # -- 
-echo $SQLdev
 
 export USER='root'
 export PASSWORD=\"$DB_PASSWORD\" 
@@ -15,4 +14,6 @@ echo $USER
 echo $PASSWORD
 echo $DATABASE_URL
 
-#cd app-code && ./mvnw test
+#
+
+cd app-code && ./mvnw test
